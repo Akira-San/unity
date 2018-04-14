c29fb7af097cb0e66dfb641060cf30c8516d1f72
Akira-San <38386020+Akira-San@users.noreply.github.com>
2018-04-15 04:15:14 +0800

    Add files via upload

---
+++ PacMan.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMan : MonoBehaviour
+{
+    public float speed = 1f;
+    private Vector2 dest = Vector2.zero;
+    Rigidbody2D rb2D;
+    public int point=29;
+    private int score;
+
+
+    void Start()
+    {
+        dest = Vector2.right;
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector2 pos = transform.position;
+        transform.Translate(dest*speed*Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            dest = Vector2.up;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            dest = Vector2.right;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            dest = -Vector2.up;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            dest = -Vector2.right;
+
+        GetComponent<Animator>().SetFloat("DirX", dest.x);
+        GetComponent<Animator>().SetFloat("DirY", dest.y);
+
+        if (pos.x >= 14f)
+            transform.position = new Vector2(pos.x - 28f, pos.y);
+        if (pos.x <= -14f)
+            transform.position = new Vector2(pos.x + 28f, pos.y);
+
+        GameObject son = GameObject.Find("pacmanx");
+
+        if (pos.x >= 10f || pos.x <= -10f || pos.y >= 11f || pos.y <= -11f)
+            son.transform.position = pos - 4 * dest;
+        else
+            son.transform.position = pos + 4 * dest;
+    }
+ 
+    bool Check(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos+dir , pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.name == "point")
+        {
+            Destroy(other.gameObject);
+            score = score + 10;
+            point = point + 1;
+        }
+        else if(other.name=="big")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("big");
+            score = score + 50;
+        }
+
+    }
+
+    }
+
---
+++ Red.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Red : MonoBehaviour
+{
+    public Transform[] waypoints;
+    int cur = 0;
+    public float speed = 0.3f;
+
+    void FixedUpdate()
+    {
+        GameObject player = GameObject.Find("pacman");
+        int point= player.GetComponent<PacMan>().point;
+        Vector2 dest = transform.position;
+
+        Vector2 pos = player.transform.position - transform.position;
+
+        if (dest.x >= 14f)
+            transform.position = new Vector2(dest.x - 28f, dest.y);
+        else if (dest.x <= -14f)
+            transform.position = new Vector2(dest.x + 28f, dest.y);
+
+        GetComponent<Animator>().SetFloat("DirX", pos.x);
+        GetComponent<Animator>().SetFloat("DirY", pos.y);
+
+        if (point < 30)
+        {
+            if (transform.position != waypoints[cur].position)
+            {
+                Vector2 p = Vector2.MoveTowards(transform.position,
+                                                waypoints[cur].position,
+                                                speed);
+                GetComponent<Rigidbody2D>().MovePosition(p);
+            }
+            // Waypoint reached, select next one
+            else cur = (cur + 1) % waypoints.Length;
+        }
+        else
+            GetComponent<AILerp>().canSearch=true;
+    }
+
+}
